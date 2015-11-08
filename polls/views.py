61b067eab095f1ba248bf78c01d851bb4ec75655
Paolo Ibarra <jpibarra1130@gmail.com>
2015-11-08 22:14:48 +0800

    Added detail, index and results implementation.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,40 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, Http404
+from django.core.urlresolvers import reverse
+from django.views import generic
 
-def index(request):
-  return HttpResponse("Hello, world. You're at the polls index.")
-  
+from .models import Question
+
+class IndexView(generic.ListView):
+  template_name = 'polls/index.html'
+  context_object_name = 'latest_question_list'
+
+  def get_queryset(self):
+    """Return the last five published questions."""
+    return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request, question_id):
+  p = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = p.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    # Redisplay the question voting form.
+    return render(request, 'polls/detail.html', {
+      'question': p,
+      'error_message': "You didn't select a choice.",
+      })
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    
+    # always do response redirect when handling POST data
+    return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
